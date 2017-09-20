7f669aa0502801748c66904450a996fd86246777
PeterStan <p.stanwicks@gmail.com>
2017-09-20 13:34:51 -0400

    initial commit

---
+++ CacheSim.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <assert.h>
+//#include "CacheTest.c"
+
+
+struct cache{
+int tagFieldLength;
+int setNumFieldLength;
+int blockOffsetFieldLength;
+unsigned int **tagArray; 
+int **lruArray;
+};
+
+
+int whichSet(){
+	return 0;
+}
+
+int setIndexLength(){
+	return 0;
+}
+
+int offsetLength(){
+	return 0;
+}
+
+int tagBits(){
+	return 0;
+}
+
+int hitWay(){
+	return 0;
+}
+
+int updateOnHit(){
+	return 0;
+}
+
+int updateOnMiss(){
+	return 0;
+}
+
+int main(int argc, int argv[]){
+	//argv[1]=set associativity
+	//argv[2] = line size in bytes
+	//argv[3] = total cache size in kbytes
+	//run file >a.out K L C traceFile
+
+
+	return 0;
+}
---
+++ CacheTest.c
@@ -0,0 +1,36 @@
+#include <assert.h>
+//#include "CacheSim.c"
+
+int whichSetTest(){
+	assert(whichSet() == 0);
+	return 0;
+}
+
+int setIndexLengthTest(){
+	return 0;
+}
+
+int offsetLengthTest(){
+	return 0;
+}
+
+int tagBitsTest(){
+	return 0;
+}
+
+int hitWayTest(){
+	return 0;
+}
+
+int updateOnHitTest(){
+	return 0;
+}
+
+int updateOnMissTest(){
+	return 0;
+}
+
+int testAll(void){
+
+	return 0;
+}
